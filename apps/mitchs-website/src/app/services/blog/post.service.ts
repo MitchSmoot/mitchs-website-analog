2617f9f9bedba9ff6daa6328abfe5166949e0536
MitchSmoot <mitchsmoot@gmail.com>
2023-05-12 23:30:54 -0600

    feat: added placeholder api requests for posts

--- apps/mitchs-website/src/app/services/blog/post.service.ts
+++ apps/mitchs-website/src/app/services/blog/post.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+export interface Post {
+  id: number
+  content: string
+}
+
+export interface Filter {
+  filter: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  get(filter: Filter) {
+    if (filter) {
+      console.log("Getting posts, filtered by " + filter)
+    } else {
+      console.log("Getting all posts")
+    }
+  }
+
+  getbyId(id: number) {
+    console.log("GET post" + id.toString())
+  }
+
+  create(post: Post) {
+    console.log("Creating Post:" + post.toString())
+  }
+
+  update(id: number, post: Post) {
+    console.log(`Updating Post ${id}, content: ` + post.toString())
+  }
+
+  delete(id: number) {
+    console.log("Deleting Post " + id.toString())
+  }
 }
