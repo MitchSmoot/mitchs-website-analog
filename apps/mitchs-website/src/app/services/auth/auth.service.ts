5395b3e95def07d43daab781cd9071ab63e8507a
MitchSmoot <mitchsmoot@gmail.com>
2023-05-21 22:52:20 -0600

    working: vite config

--- apps/mitchs-website/src/app/services/auth/auth.service.ts
+++ apps/mitchs-website/src/app/services/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, computed, signal } from '@angular/core';
 import { createClient } from '@supabase/supabase-js';
 import { User } from '@prisma/client';
 
-const supabase = createClient(import.meta.env['VITE_SUPABASE_URL'], import.meta.env['VITE_SUPABASE_KEY'])
+// const supabase = createClient(import.meta.env['VITE_SUPABASE_URL'], import.meta.env['VITE_SUPABASE_KEY'])
 
 @Injectable({
   providedIn: 'root'
@@ -13,18 +13,18 @@ export class AuthService {
     #currentUser = signal<User| undefined>(undefined);
     currentUser = computed(this.#currentUser);
 
-  async userSignUp() {
-    const user: { data: any, error: any } = await supabase.auth.signUp({
-      email: 'example@email.com',
-      password: 'example-password',
-    })
-    console.log(user)
-  }
+  // async userSignUp() {
+  //   const user: { data: any, error: any } = await supabase.auth.signUp({
+  //     email: 'example@email.com',
+  //     password: 'example-password',
+  //   })
+  //   console.log(user)
+  // }
 
-  async userSignIn(email: string, password: string) {
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    })
-  }
+  // async userSignIn(email: string, password: string) {
+  //   const { data, error } = await supabase.auth.signInWithPassword({
+  //     email: email,
+  //     password: password,
+  //   })
+  // }
 }
--- apps/mitchs-website/src/main.ts
+++ apps/mitchs-website/src/main.ts
@@ -5,3 +5,5 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
+
+console.log(import.meta.env.VITE_DATABASE_URL)
