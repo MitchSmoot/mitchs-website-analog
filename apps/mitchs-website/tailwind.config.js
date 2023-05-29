4f33414b99c4e38705adb34dff46c0fee254fa69
MitchSmoot <mitchsmoot@gmail.com>
2023-05-29 00:47:44 -0600

    feat: Tailwind works now

--- apps/mitchs-website/tailwind.config.js
+++ apps/mitchs-website/tailwind.config.js
@@ -1,14 +1,12 @@
-const { createGlobPatternsForDependencies } = require("@nx/angular/tailwind");
-const { join } = require("path");
+import { createGlobPatternsForDependencies } from "@nx/angular/tailwind";
+import { join } from "path";
 
 /** @type {import("tailwindcss").Config} */
-module.exports = {
-  darkMode: 'class',
-  content: ["./index.html",
-    join(__dirname, "src/**/!(*.stories|*.spec).{ts,html}"),
-    ...createGlobPatternsForDependencies(__dirname)
-  ],
-  theme: {
-    extend: {}
-  }
+export const darkMode = 'class';
+export const content = ["./index.html",
+  join(__dirname, "src/**/!(*.stories|*.spec).{ts,html}"),
+  ...createGlobPatternsForDependencies(__dirname)
+];
+export const theme = {
+  extend: {}
 };
