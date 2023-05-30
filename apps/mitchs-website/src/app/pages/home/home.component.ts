8aa2b73bf97641a95c46b5ae175fc512291a67dd
MitchSmoot <mitchsmoot@gmail.com>
2023-05-30 03:18:32 -0600

    added some basic tailwind styles and sections to expand upon

--- apps/mitchs-website/src/app/pages/home/home.component.ts
+++ apps/mitchs-website/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'mitchs-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
