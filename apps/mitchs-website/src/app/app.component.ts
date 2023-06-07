b55d0942e5eb38bd8663fab30efe10793c4a9dd5
MitchSmoot <mitchsmoot@gmail.com>
2023-06-07 13:28:11 -0600

    updated routerOutlet import

--- apps/mitchs-website/src/app/app.component.ts
+++ apps/mitchs-website/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { MainNavComponent } from './main-nav/main-nav.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MainNavComponent],
+  imports: [RouterOutlet, MainNavComponent],
   selector: 'mitchs-website-analog-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
