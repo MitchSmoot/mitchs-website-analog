d37c6aa14c8d593f4c5e50e4536a2b020db79ddf
MitchSmoot <mitchsmoot@gmail.com>
2023-06-27 05:34:54 -0600

    feat: fully implemented dark mode switching

--- apps/mitchs-website/src/app/main-nav/main-nav.component.ts
+++ apps/mitchs-website/src/app/main-nav/main-nav.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { AppService } from '../services/app.service';
 
 @Component({
   selector: 'main-nav',
@@ -9,4 +10,10 @@ import { RouterModule } from '@angular/router';
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss'],
 })
-export class MainNavComponent {}
+export class MainNavComponent {
+  constructor(private appService: AppService) {}
+
+  darkModeToggle() {
+    this.appService.toggleDarkMode();
+  }
+}
--- apps/mitchs-website/src/app/services/app.service.ts
+++ apps/mitchs-website/src/app/services/app.service.ts
@@ -1,16 +1,25 @@
 import { DOCUMENT } from '@angular/common';
-import { Injectable, PLATFORM_ID, RendererFactory2, inject, signal } from '@angular/core';
+import { Injectable, RendererFactory2, inject, signal, effect } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  private platformId = inject(PLATFORM_ID);
   private renderer = inject(RendererFactory2).createRenderer(null, null);
   private document = inject(DOCUMENT);
 
-  darkMode = signal<boolean>(true);
+  #darkMode = signal<boolean>(JSON.parse(localStorage.getItem('darkMode') ?? 'true'));
+  
+  updateDarkClassInHTML = effect(() => {
+    this.#darkMode() ?
+      this.renderer.addClass(this.document.body, 'dark') :
+      this.renderer.removeClass(this.document.body, 'dark')
+    }
+  );
 
-  constructor() { }
+  toggleDarkMode() {
+    this.#darkMode.set(!this.#darkMode());
+    localStorage.setItem('darkMode', JSON.stringify(this.#darkMode()));
+  }
 }
