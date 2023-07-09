75da94b7e5f85c089976c91b21c318407fd6912c
MitchSmoot <mitchsmoot@gmail.com>
2023-07-08 23:36:55 -0600

    content: cave formation repair project page

---
+++ apps/mitchs-website/src/app/pages/caving/cave-repair/(cave-repair).page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'mitchs-cave-repair-homepage',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  template: `
+  <h2>Cave Formation Project</h2>
+  <p>
+    The Cave Formation Repair Project is a project to repair cave formations that have been damaged by humans.
+  </p>
+  <p>
+    THe project begain in June 2018 in New Mexico, and has since been expanded to a few other caves, including the Lehman caves, in Nevada's Great Basin National Park.
+  </p>
+  `
+})
+export default class CaveRepairPageComponent {}
