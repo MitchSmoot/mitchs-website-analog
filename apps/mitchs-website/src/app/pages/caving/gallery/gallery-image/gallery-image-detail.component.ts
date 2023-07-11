62d1a242049bffd8b97c44c5f8cffd558ba0de50
MitchSmoot <mitchsmoot@gmail.com>
2023-07-10 23:56:16 -0600

    created components for gallery

---
+++ apps/mitchs-website/src/app/pages/caving/gallery/gallery-image/gallery-image-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "mitchs-gallery-image-detail",
+  standalone: true,
+  template: `
+    <h2>Gallery Image in Detail</h2>
+    `,
+})
+export default class GalleryImageDetailComponent {}
---
+++ apps/mitchs-website/src/app/pages/caving/gallery/gallery-image/gallery-image.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "mitchs-gallery-image",
+  standalone: true,
+  template: `
+    <h2>Gallery Image</h2>
+    `,
+})
+export default class GalleryImageComponent {}
