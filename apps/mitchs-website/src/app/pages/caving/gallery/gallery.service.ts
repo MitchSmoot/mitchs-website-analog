074ea58e7ad8a6d64fda5f5c90a8804aa0527a71
MitchSmoot <mitchsmoot@gmail.com>
2023-11-04 15:39:17 -0600

    uninstalled tailwindCSS and dependencies

--- apps/mitchs-website/src/app/pages/caving/gallery/gallery.service.ts
+++ apps/mitchs-website/src/app/pages/caving/gallery/gallery.service.ts
@@ -3,6 +3,7 @@ import { Injectable, signal } from "@angular/core";
 export interface galleryImage {
   title: string;
   url: string;
+  type: string;
   thumbnailUrl?: string;
   description?: string;
   location?: string;
@@ -20,72 +21,84 @@ export default class GalleryService {
     {
       title: "Black Stream",
       url: "/img/gallery1.jpg",
+      type: "img",
       thumbnailUrl: "/img/gallery1t.jpg",
       description: 'This is an underground river known as "Black Stream" located deep within Utah\'s longest cave. ',
     },
     {
       title: "Image 2",
       url: "/img/gallery2.jpg",
+      type: "img",
       thumbnailUrl: "/img/gallery2.jpg",
       description: "This is a description of image 2",
     },
     {
       title: "Image 3",
       url: "/img/gallery3.jpg",
+      type: "img",
       thumbnailUrl: "/img/gallery3.jpg",
       description: "This is a description of image 3",
     },
     {
       title: "Image 4",
       url: "/img/gallery4.jpg",
+      type: "img",
       thumbnailUrl: "/img/gallery4.jpg",
       description: "This is a description of image 4",
     },
     {
       title: "Image 5",
       url: "/img/gallery2.jpg",
+      type: "img",
       thumbnailUrl: "/img/gallery2.jpg",
       description: "This is a description of image 1",
     },
     {
       title: "Image 6",
       url: "/img/gallery3.jpg",
+      type: "img",
       thumbnailUrl: "/img/gallery3.jpg",
       description: "This is a description of image 2",
     },
     {
       title: "Image 7",
       url: "/img/gallery1.jpg",
+      type: "img",
       thumbnailUrl: "/img/gallery1.jpg",
       description: "This is a description of image 3",
     },
     {
       title: "Image 8",
       url: "/img/gallery4.jpg",
+      type: "img",
       thumbnailUrl: "/img/gallery4.jpg",
       description: "This is a description of image 4",
     },
     {
       title: "Image 9",
       url: "/img/gallery4.jpg",
+      type: "img",
       thumbnailUrl: "/img/gallery4.jpg",
       description: "This is a description of image 1",
     },
     {
       title: "Image 10",
       url: "/img/gallery2.jpg",
+      type: "img",
       thumbnailUrl: "/img/gallery2.jpg",
       description: "This is a description of image 2",
     },
     {
       title: "Image 11",
       url: "/img/gallery1.jpg",
+      type: "img",
       thumbnailUrl: "/img/gallery1.jpg",
       description: "This is a description of image 3",
     },
     {
       title: "Image 12",
       url: "/img/gallery3.jpg",
+      type: "img",
       thumbnailUrl: "/img/gallery3.jpg",
       description: "This is a description of image 4",
     }
