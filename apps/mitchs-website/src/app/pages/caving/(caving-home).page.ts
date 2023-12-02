2d304b80b3d69c38830c83fe21dd26f382a6b968
MitchSmoot <mitchsmoot@gmail.com>
2023-12-01 23:52:01 -0700

    updated text content

--- apps/mitchs-website/src/app/pages/caving/(caving-home).page.ts
+++ apps/mitchs-website/src/app/pages/caving/(caving-home).page.ts
@@ -12,7 +12,7 @@ import GalleryService from './gallery/gallery.service';
   <h4>Exploring the Unknown, Protecting the Known</h4>
   <article>
     <p>
-      Caving is an exhilarating adventure activity that takes individuals into the hidden depths of the Earth's intricate cave systems. It offers a unique opportunity to explore and appreciate the natural wonders that lie beneath the surface. As cave enthusiasts venture into the dark and mysterious passages, they encounter breathtaking geological formations, such as stalagmites and stalactites, intricate underground rivers, and fascinating ecosystems that thrive in the absence of light. Caving requires a combination of physical agility, mental resilience, and a deep sense of curiosity to navigate the challenges posed by narrow passages, uneven terrain, and occasional tight squeezes. It provides an unparalleled sense of connection with the Earth's geological history and offers an unforgettable experience of exploration and discovery.
+      Welcome to my personal caving site! I am a caver based in the Western US, and I love exploring caves and learning about the underground world. I am also passionate about protecting caves and their unique environments.
     </p>
 
   </article>
@@ -25,7 +25,7 @@ import GalleryService from './gallery/gallery.service';
     <article>
       <h3>Caving Responsibly</h3>
       <p>
-      According to the National Speleological Society's safety standards, practicing safe caving involves several key guidelines. Firstly, it's essential to have proper training and knowledge of cave exploration techniques. Always explore caves with a group, as solo caving is strongly discouraged. Before entering a cave, inform someone on the surface about your plans and expected return time. Wear appropriate safety gear, including a helmet with a mounted light source, sturdy footwear, and protective clothing. Carry essential equipment like extra light sources, a first aid kit, a map or guidebook, and sufficient food and water. Stay aware of potential hazards such as loose rocks, slippery surfaces, and potential flooding. Finally, respect the cave environment by leaving no trace and avoiding damage to fragile formations.
+        Caving, while a fun and exciting activity, can be dangerous. It is important to come prepared with the right gear and knowledge. Caves are also very delicate, and must be protected for future generations. 
       </p>
       <a routerLink="/caving/gear" routerLinkActive="active">Lear more about caving gear</a>
     </article>
