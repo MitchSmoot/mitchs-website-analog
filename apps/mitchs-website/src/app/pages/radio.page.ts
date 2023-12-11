a28388c020ca06beb243db7d58a0d9e397accca0
mitchsmoot <mitchsmoot@gmail.com>
2023-12-10 23:23:25 -0700

    working: highcharts

--- apps/mitchs-website/src/app/pages/radio.page.ts
+++ apps/mitchs-website/src/app/pages/radio.page.ts
@@ -2,16 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NoaaService } from '../services/noaa.service';
+import { HighchartsChartModule } from 'highcharts-angular';
+
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'mitchs-radio-page',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HighchartsChartModule],
   templateUrl: './radio/radio-page.component.html',
   styleUrls: ['./radio/radio-page.component.scss'],
 })
 export default class RadioPageComponent implements OnInit {
   constructor(private noaaService: NoaaService) {}
+
+  // Highcharts: typeof Highcharts = Highcharts;
+  // chartConstructor: string = 'chart';
+  // chartOptions: Highcharts.Options = { };
+  // updateFlag: boolean = false;
+  // oneToOneFlag: boolean = true;
+
 
   ngOnInit() {
     console.log("init");
