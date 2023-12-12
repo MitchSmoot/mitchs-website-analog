f772b38dc586038f7f9fbc0357848adf05a2d135
mitchsmoot <mitchsmoot@gmail.com>
2023-12-11 18:40:58 -0700

    hid unfinished caving pages

--- apps/mitchs-website/src/app/pages/caving.page.ts
+++ apps/mitchs-website/src/app/pages/caving.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HideinProdDirective } from '../shared/hide-in-prod.directive';
 
 @Component({
   selector: 'mitchs-caving-page',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HideinProdDirective],
   templateUrl: './caving-page.component.html',
   styleUrls: ['./caving/caving.scss'],
 })
