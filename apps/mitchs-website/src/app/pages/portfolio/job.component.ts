95b0c1eb625983c2beaf3b99ce5463b29507c79c
MitchSmoot <mitchsmoot@gmail.com>
2024-01-20 06:23:30 -0700

    chore: updated to angular 17.1.0

---
+++ apps/mitchs-website/src/app/pages/portfolio/job.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'job',
+  standalone: true,
+  imports: [],
+  templateUrl: './job.component.html',
+  styleUrls: ['./portfolio.scss'],
+})
+export class JobComponent {
+}
