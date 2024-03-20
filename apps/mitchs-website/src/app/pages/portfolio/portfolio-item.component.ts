8844d42127b4abc0aeccc409f0929c0d57baf4c9
MitchSmoot <mitchsmoot@gmail.com>
2024-03-19 18:32:55 -0600

    workaround for broken pages

--- apps/mitchs-website/src/app/pages/portfolio/portfolio-item.component.ts
+++ apps/mitchs-website/src/app/pages/portfolio/portfolio-item.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PortfolioItem } from './portfolio.service';
 import { TechIconComponent } from './tech-icon.component';
 
 @Component({
   selector: 'mitchs-portfolio-item',
   standalone: true,
-  imports: [CommonModule, TechIconComponent],
+  imports: [TechIconComponent],
   templateUrl: './portfolio-item.component.html',
   styleUrls: ['./portfolio.scss'],
 })
