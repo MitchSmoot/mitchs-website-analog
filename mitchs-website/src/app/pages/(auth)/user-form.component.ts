f873abc123770d0298c04569bf5bd5716dd02063
MitchSmoot <mitchsmoot@gmail.com>
2024-03-27 15:22:03 -0600

    fixed type errors

--- mitchs-website/src/app/pages/(auth)/user-form.component.ts
+++ mitchs-website/src/app/pages/(auth)/user-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../../services/auth/auth.service';
+import { AuthService } from '../../services/auth.service';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
--- mitchs-website/src/main.server.ts
+++ mitchs-website/src/main.server.ts
@@ -6,13 +6,12 @@ import { renderApplication } from '@angular/platform-server';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
 
 if (import.meta.env.PROD) {
   enableProdMode();
 }
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = () => bootstrapApplication(AppComponent);
 
 export default async function render(url: string, document: string) {
   const html = await renderApplication(bootstrap, {
