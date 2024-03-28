be77c38dae2024511bba44413b34786a3e29552a
MitchSmoot <mitchsmoot@gmail.com>
2024-03-27 22:07:09 -0600

    added vercel preset to nitro config

--- mitchs-website/vite.config.ts
+++ mitchs-website/vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig(({ mode }) => {
       analog({
         ssr: false,
         nitro: {
+          preset: 'vercel',
           routeRules: {
             '/': {
               prerender: false,
