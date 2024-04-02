e47688e747d1675eac2eb23033387c1d265a487d
MitchSmoot <mitchsmoot@gmail.com>
2024-04-01 23:59:18 -0600

    edited output directory

--- mitchs-website/vite.config.ts
+++ mitchs-website/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     root: __dirname,
     publicDir: 'src/public',
     build: {
-      outDir: '../dist/mitchs-website/client',
+      outDir: './dist/mitchs-website/client',
       reportCompressedSize: true,
       commonjsOptions: { transformMixedEsModules: true },      
       target: ['es2020'],
