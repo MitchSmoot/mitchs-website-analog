98600f3fc0e43b216f2cc9eb5ce57df3a7b57195
MitchSmoot <mitchsmoot@gmail.com>
2024-04-05 19:30:19 -0600

    edited vite config

--- mitchs-website/vite.config.ts
+++ mitchs-website/vite.config.ts
@@ -31,8 +31,6 @@ export default defineConfig(({ mode }) => {
     },    
     plugins: [
       analog({
-        ssr: false,
-        static: true,
         prerender: {
           routes: [
             '/'
