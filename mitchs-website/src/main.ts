9e556fd568c9484a4c59a38e62e95fe37d6dd08b
MitchSmoot <mitchsmoot@gmail.com>
2024-04-10 22:51:40 -0600

    set up vercel analytics

--- mitchs-website/src/main.ts
+++ mitchs-website/src/main.ts
@@ -4,6 +4,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
+// inject vercel analytics
+import { inject } from '@vercel/analytics';
+inject();
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
