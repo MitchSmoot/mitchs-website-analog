a17035b7ba460f991e9f302a2b406680e6ce950e
MitchSmoot <mitchsmoot@gmail.com>
2024-04-16 23:59:03 -0600

    created page for freelance work

---
+++ mitchs-website/src/app/pages/freelance/freelance.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+    selector: 'mitchs-freelance-page',
+    standalone: true,
+    templateUrl: './caving-home.component.html',
+    imports: [CommonModule, RouterModule],
+    styleUrls: ['./caving.scss']
+})
+export default class FreelancePageComponent {
+}
